58147b17228a6b5cbab8db9a4c9e1d5f69fb8eab
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-05-06 03:45:46 +0530

    8th stage

--- src/Models/DNSMessage.cs
+++ src/Models/DNSMessage.cs
@@ -70,7 +70,7 @@ namespace codecrafters_dns_server.src.Models
 
             Console.WriteLine("Offset: " + offset);
             var answers = new List<DNSAnswer>();
-            offset -= 5;
+            offset -= 6;
             for (int i = 0; i < header.AnswerRecordCount; i++)
             {
                 var q = DNSAnswer.Read(buffer[offset..], out offset);
