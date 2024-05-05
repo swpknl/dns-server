384f6f154b93d0a71e2023da5a9045559289f60a
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-05-05 13:09:37 +0530

    pass 1st stage

--- src/Server.cs
+++ src/Server.cs
@@ -8,26 +8,26 @@ Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 // // Resolve UDP address
-// IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
-// int port = 2053;
-// IPEndPoint udpEndPoint = new IPEndPoint(ipAddress, port);
-// 
-// // Create UDP socket
-// UdpClient udpClient = new UdpClient(udpEndPoint);
-// 
-// while (true)
-// {
-//     // Receive data
-//     IPEndPoint sourceEndPoint = new IPEndPoint(IPAddress.Any, 0);
-//     byte[] receivedData = udpClient.Receive(ref sourceEndPoint);
-//     string receivedString = Encoding.ASCII.GetString(receivedData);
-// 
-//     Console.WriteLine($"Received {receivedData.Length} bytes from {sourceEndPoint}: {receivedString}");
-// 
-//     // Create an empty response
-//     byte[] response = Encoding.ASCII.GetBytes("");
-// 
-//     // Send response
-//     udpClient.Send(response, response.Length, sourceEndPoint);
-// }
+IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+int port = 2053;
+IPEndPoint udpEndPoint = new IPEndPoint(ipAddress, port);
+
+// Create UDP socket
+UdpClient udpClient = new UdpClient(udpEndPoint);
+
+while (true)
+{
+    // Receive data
+    IPEndPoint sourceEndPoint = new IPEndPoint(IPAddress.Any, 0);
+    byte[] receivedData = udpClient.Receive(ref sourceEndPoint);
+    string receivedString = Encoding.ASCII.GetString(receivedData);
+
+    Console.WriteLine($"Received {receivedData.Length} bytes from {sourceEndPoint}: {receivedString}");
+
+    // Create an empty response
+    byte[] response = Encoding.ASCII.GetBytes("");
+
+    // Send response
+    udpClient.Send(response, response.Length, sourceEndPoint);
+}
 
