c1ff66bf1aa9c188d5d0a2a65c63f57fefd043c4
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-05-06 02:11:36 +0530

    8th stage

---
+++ src/Models/DNSParser.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_dns_server.src.Models
+{
+    public class DNSParser
+    {
+        private byte[] request;
+        public int id, nq;
+        byte[] header = new byte[12];
+
+        public DNSParser(byte[] req)
+        {
+            request = req;
+            id = request[0] << 8 + request[1];
+            nq = (request[4] << 8) | request[5];
+        }
+
+        public byte[] getHeader()
+        {
+
+            header[0] = request[0];
+            header[1] = request[1];
+
+            int aa = 0, opcode = (request[2] >> 3) & 0x0F, rd = request[2] & 0x01;
+            header[2] = (byte)((1 << 7) | (opcode << 3) | (aa << 2) | (0 << 1) | rd);
+
+            int ra = 0, rz = 0, rc = 0;
+            if (opcode != 0)
+                rc = 4;
+
+            header[3] = (byte)((ra << 7) | (rz << 4) | rc);
+
+            int na = nq;
+            header[4] = (byte)(nq >> 8);
+            header[5] = (byte)(nq & 0xFF);
+            header[6] = (byte)(na >> 8);
+            header[7] = (byte)(na & 0xFF);
+
+            int nra = 0, ara = 0;
+            header[8] = (byte)(nra >> 8);
+            header[9] = (byte)(nra & 0xFF);
+            header[10] = (byte)(ara >> 8);
+            header[11] = (byte)(ara & 0xFF);
+
+            return header;
+        }
+
+        public int populateQuestion(int reqIdx, List<byte> questions)
+        {
+            int labelLength = request[reqIdx];
+            while (labelLength != 0)
+            {
+                if ((labelLength & 0xC0) == 0xC0)
+                {
+                    int idx = (labelLength & 0x3F << 8) | request[reqIdx + 1];
+                    reqIdx += 2;
+                    labelLength = request[idx];
+                    while (labelLength != 0)
+                    {
+                        for (int j = 0; j <= labelLength; j++)
+                        {
+                            questions.Add(request[idx++]);
+                        }
+                        labelLength = request[idx];
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j <= labelLength; j++)
+                    {
+                        questions.Add(request[reqIdx++]);
+                    }
+                }
+                labelLength = request[reqIdx];
+            }
+            questions.Add(0);
+
+            Utility.addBigEndianToList(questions, 1, 2);
+            Utility.addBigEndianToList(questions, 1, 2);
+            reqIdx += 5;
+            return reqIdx;
+        }
+
+        public byte[] getQuestion()
+        {
+            List<byte> questions = new List<byte>();
+            int reqIdx = 12;
+            try
+            {
+                for (int i = 0; i < nq; i++)
+                {
+                    reqIdx = populateQuestion(reqIdx, questions);
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Index Out of Bound idx: " + reqIdx);
+            }
+            return questions.ToArray();
+        }
+
+        public byte[] getAnswers(uint[]? queriedIps)
+        {
+            List<byte> answers = new List<byte>();
+            int reqIdx = 12;
+            try
+            {
+                for (int i = 0; i < nq; i++)
+                {
+                    reqIdx = populateQuestion(reqIdx, answers);
+
+                    int ttl = 60, length = 4;
+                    uint ip = 123456;
+
+                    if (queriedIps != null && queriedIps.Length > i)
+                    {
+                        ip = queriedIps[i];
+                    }
+
+                    Utility.addBigEndianToList(answers, ttl, 4);
+                    Utility.addBigEndianToList(answers, length, 2);
+                    Utility.addBigEndianToList(answers, ip, 4);
+
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.Error.WriteLine("Errro: Malformed Request Recieved should be loneger than: " + reqIdx);
+            }
+
+            return answers.ToArray();
+        }
+
+        public byte[] getResponse(uint[]? queriedIps = null)
+        {
+            getHeader();
+            byte[] question = getQuestion();
+            byte[] answers = getAnswers(queriedIps);
+
+            byte[] response = new byte[header.Length + question.Length + answers.Length];
+
+            Array.Copy(header, 0, response, 0, header.Length);
+            Array.Copy(question, 0, response, header.Length, question.Length);
+            Array.Copy(answers, 0, response, header.Length + question.Length, answers.Length);
+
+            Console.WriteLine($"Sending Response: {response.Length}");
+            Utility.print2Hex(response);
+
+            // Utility.WriteBinaryDataToFile(response, "bin.bin");
+
+            return response;
+        }
+    }
+}
---
+++ src/Models/Utility.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_dns_server.src.Models
+{
+    public static class Utility
+    {
+        public static void print2Hex(byte[] array)
+        {
+            string hexRepresentation = BitConverter.ToString(array).Replace("-", " ");
+            string asciiRepresentaiton = ByteArrayToAsciiString(array);
+            int length = hexRepresentation.Length;
+            for (int i = 0; i < array.Length; i += 8)
+            {
+                Console.Write(hexRepresentation.Substring(3 * i, Math.Min(24, length - 3 * i)));
+                if (3 * i + 24 > length)
+                {
+                    int j = 3 * i + 24 - length + 1;
+                    for (; j > 0; j--)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine(" " + asciiRepresentaiton.Substring(2 * i, Math.Min(16, asciiRepresentaiton.Length - 2 * i)));
+            }
+        }
+
+        public static void addBigEndianToList(List<byte> list, int value, int length)
+        {
+            for (int i = length - 1; i >= 0; i--)
+            {
+                list.Add((byte)((value >> (i * 8)) & 0xFF));
+            }
+        }
+
+        public static void addBigEndianToList(List<byte> list, uint value, int length)
+        {
+            for (int i = length - 1; i >= 0; i--)
+            {
+                list.Add((byte)((value >> (i * 8)) & 0xFF));
+            }
+        }
+
+        static bool IsPrintableAscii(byte b)
+        {
+            return b >= 32 && b <= 126; // Printable ASCII range
+        }
+
+
+        public static string ByteArrayToAsciiString(byte[] byteArray)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (byte b in byteArray)
+            {
+                if (IsPrintableAscii(b))
+                {
+                    sb.Append((char)b);
+                }
+                else
+                {
+                    sb.Append(".");
+                }
+                sb.Append(" ");
+            }
+
+            return sb.ToString();
+        }
+
+        public static IPEndPoint? getIpFromString(string resolverAddress)
+        {
+            string[] parts = resolverAddress.Split(':');
+            if (parts.Length == 2 && IPAddress.TryParse(parts[0], out IPAddress? ipAddress) && int.TryParse(parts[1], out int port))
+            {
+                return new IPEndPoint(ipAddress, port);
+            }
+            return null;
+        }
+
+
+        public static void WriteBinaryDataToFile(byte[] binaryData, string filePath)
+        {
+            // Open a file stream for writing
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                // Write the binary data to the file
+                fileStream.Write(binaryData, 0, binaryData.Length);
+            }
+        }
+    }
+}
