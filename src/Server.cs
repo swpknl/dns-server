170f52d2ac3a2535a1dd4b233b9bd28276e16bb7
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-05-05 23:37:54 +0530

    8th stage

--- src/Server.cs
+++ src/Server.cs
@@ -56,6 +56,7 @@ while (true)
         var resolverResponse = await resolverUdpClient.ReceiveAsync();
         response = resolverResponse.Buffer;
         Console.WriteLine("Response : " + Encoding.UTF8.GetString(response));
+        resolverUdpClient = null;
         await udpClient.SendAsync(response, sourceEndPoint);
     }
     else
